172f998618fa6fdbcd65ad775dd5896bf92201a5
KalaivaniKalaivani <144912811+KalaivaniKalaivani@users.noreply.github.com>
2025-03-17 15:34:03 +0530

    Create novalnet_lang.php

---
+++ views/admin_twig/en/novalnet_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$sLangName = 'English';
+
+$aLang = [
+    'charset'                                           => 'UTF-8',
+    'TAB_NOVALNET_ORDERS'                                 => 'Novalnet',
+    'SHOP_MODULE_GROUP_novalnetGlobalSettings'          => 'Novalnet API Configuration',
+    'SHOP_MODULE_sProductActivationKey'                 => 'Product activation key',
+    'HELP_SHOP_MODULE_sProductActivationKey'            => 'Get your Product activation key from the <a class="novalnet_config_link" href="https://admin.novalnet.de" target="_blank">Novalnet Admin Portal</a> Project > Choose your project > API credentials >API Signature (Product activation key)',
+    'SHOP_MODULE_sPaymentAccessKey'                     => 'Payment access key',
+    'HELP_SHOP_MODULE_sPaymentAccessKey'                => 'Get your Payment access key from the <a class="novalnet_config_link" href="https://admin.novalnet.de" target="_blank">Novalnet Admin Portal</a> Project > Choose your project > API credentials >Payment access key',
+    'SHOP_MODULE_sTariffId'                             => 'Select Tariff ID',
+    'HELP_SHOP_MODULE_sTariffId'                        => 'Select a Tariff ID to match the preferred tariff plan you created at the Novalnet Admin Portal for this project ',
+    'SHOP_MODULE_sTariffId_None'                        => 'None',
+    'SHOP_MODULE_GROUP_novalnetGlobalSettingsWebhook'   => 'Notification / Webhook URL Setup',
+    'SHOP_MODULE_blWebhookNotification'                 => 'Allow manual testing of the Notification / Webhook URL',
+    'HELP_SHOP_MODULE_blWebhookNotification'            => 'Enable this to test the Novalnet Notification / Webhook URL manually. Disable this before setting your shop live to block unauthorized calls from external parties',
+    'SHOP_MODULE_blWebhookSendMail'                     => 'Send e-mail to',
+    'HELP_MODULE_blWebhookSendMail'                     => 'Notification / Webhook URL execution messages will be sent to this e-mail',
+    'SHOP_MODULE_sWebhooksUrl'                          => 'Notification / Webhook URL'];
