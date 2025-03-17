f16d845e86fd92bb5396e72cc546c4d4e093ebb8
KalaivaniKalaivani <144912811+KalaivaniKalaivani@users.noreply.github.com>
2025-03-17 15:26:44 +0530

    Create ViewConfig.php

---
+++ Core/ViewConfig.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Novalnet payment module
+ *
+ * This file is used for assigning the view values.
+ *
+ * @author    Novalnet AG
+ * @copyright Copyright by Novalnet
+ * @license   https://www.novalnet.de/payment-plugins/kostenlos/lizenz
+ *
+ * Script: ViewConfig.php
+ */
+
+namespace Novalnet\Core;
+
+use OxidEsales\Eshop\Core\Registry;
+
+/**
+ * Class ViewConfig.
+ */
+class ViewConfig extends ViewConfig_parent
+{
+
+   
+}
