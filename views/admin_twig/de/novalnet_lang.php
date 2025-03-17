2f8b62dc602c81a898b0f94b82139ccc94f26f27
KalaivaniKalaivani <144912811+KalaivaniKalaivani@users.noreply.github.com>
2025-03-17 15:35:15 +0530

    Create novalnet_lang.php

---
+++ views/admin_twig/de/novalnet_lang.php
@@ -0,0 +1,22 @@
+<?php
+$sLangName = 'Deutsch';
+
+$aLang = [
+    'charset'                                           => 'UTF-8',
+    'TAB_NOVALNET_ORDERS'                                 => 'Novalnet',
+    'SHOP_MODULE_GROUP_novalnetGlobalSettings'          => 'Novalnet API-Konfiguration',
+    'SHOP_MODULE_sProductActivationKey'                 => 'Produktaktivierungsschlüssel',
+    'HELP_SHOP_MODULE_sProductActivationKey'            => 'Ihren Produktaktivierungsschlüssel finden Sie im <a class="novalnet_config_link" href="https://admin.novalnet.de" target="_blank">Novalnet Admin-Portal</a> Projekte > Wählen Sie Ihr Projekt > API-Anmeldeinformationen > API-Signatur (Produktaktivierungsschlüssel)',
+    'SHOP_MODULE_sPaymentAccessKey'                     => ' Zahlungs-Zugriffsschlüssel',
+    'HELP_SHOP_MODULE_sPaymentAccessKey'                => 'Ihren Paymentzugriffsschlüssel finden Sie im <a class="novalnet_config_link" href="https://admin.novalnet.de" target="_blank">Novalnet Admin-Portal</a> Projekte > Wählen Sie Ihr Projekt > API-Anmeldeinformationen > Paymentzugriffsschlüssel',
+    'SHOP_MODULE_sTariffId'                             => ' Auswahl der Tarif-ID',
+    'HELP_SHOP_MODULE_sTariffId'                        => 'Wählen Sie eine Tarif-ID, die dem bevorzugten Tarifplan entspricht, den Sie im Novalnet Admin-Portal für dieses Projekt erstellt haben',
+    'SHOP_MODULE_sTariffId_None'                        => 'None',
+    'SHOP_MODULE_GROUP_novalnetGlobalSettingsWebhook'   => 'Benachrichtigungs- / Webhook-URL festlegen',
+    'SHOP_MODULE_blWebhookNotification'                 => 'Manuelles Testen der Benachrichtigungs- / Webhook-URL erlauben',
+    'HELP_SHOP_MODULE_blWebhookNotification'            => 'Aktivieren Sie diese Option, um die Novalnet-Benachrichtigungs-/Webhook-URL manuell zu testen. Deaktivieren Sie die Option, bevor Sie Ihren Shop liveschalten, um unautorisierte Zugriffe von Dritten zu blockieren',
+    'SHOP_MODULE_blWebhookSendMail'                     => 'E-Mails senden an',
+    'HELP_MODULE_blWebhookSendMail'                     => 'E-Mail-Benachrichtigungen werden an diese E-Mail-Adresse gesendet',
+    'SHOP_MODULE_sWebhooksUrl'                          => 'Benachrichtigungs- / Webhook-URL',
+    'HELP_SHOP_MODULE_sWebhooksUrl'                     => 'Eine Benachrichtigungs- / Webhook-URL ist erforderlich, um die Datenbank / das System des Händlers mit dem Novalnet-Account synchronisiert zu halten (z.B. Lieferstatus). Weitere Informationen finden Sie in der Installationsanleitung'
+  ];
